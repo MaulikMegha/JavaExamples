fec24c5de44d5d103f42063b0ef636b4154db13f
mmegha <mmegha@raileurope.com>
2019-03-03 19:47:00 -0500

    java collection example

--- src/com/test/collection/list/Mainapp.java
+++ src/com/test/collection/list/Mainapp.java
@@ -1,5 +1,10 @@
 package com.test.collection.list;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mainapp {
+    List<Integer> list = new ArrayList<>();
+
 
 }
---
+++ src/com/test/collection/map/Customer.java
@@ -0,0 +1,21 @@
+package com.test.collection.map;
+
+public class Customer {
+
+    private String name;
+    private String item;
+
+    public Customer(String name,String item) {
+        this.name = name;
+        this.item = item;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getItem(){
+        return this.item;
+    }
+
+}
